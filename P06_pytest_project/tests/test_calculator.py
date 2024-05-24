3b86c50bb3f03b8a9c17c7083a59490c435e8aaf
23017978 <23017978@myrp.edu.sg>
2024-05-24 14:24:42 +0800

    initial commit

---
+++ P06_pytest_project/tests/test_calculator.py
@@ -0,0 +1,69 @@
+import pytest
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+
+    def test_subtract(self):
+        # arrange
+        a = 4000
+        b = 2000
+        cal = Calculator()
+
+        # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 2000
+        assert result == expected
+
+
+    def test_multiply(self):
+        # arrange
+        a = 4000
+        b = 2
+        cal = Calculator()
+
+        # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 8000
+        assert result == expected
+
+
+    def test_divide(self):
+        # arrange
+        a = 4000
+        b = 4
+        cal = Calculator()
+
+        # act
+        result = cal.divide(a, b)
+
+        # assert
+        expected = 1000
+        assert result == expected
+
+    
+    def test_divide_by_zero(self):
+        # arrange
+        a = 4321
+        b = 0
+        cal = Calculator()
+
+        # Act & Assert
+        with pytest.raises(ZeroDivisionError):
+            cal.divide(a, b)
